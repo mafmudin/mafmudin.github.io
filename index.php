13d023987fff88c5e5c438077df8582c19526663
mafmudin <mafmudin@gamatechno.com>
2018-08-14 10:44:25 +0700

    change to php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+	<link rel="stylesheet" href="css/mystyle.css" type="text/css" charset="utf-8">
+	<link rel="stylesheet" href="css/stylesheet.css" type="text/css" charset="utf-8" />
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<style type="text/css">
+	body{
+		font-family: 'poiret_oneregular';
+	}
+</style>
+<title>Mafmudin Git</title>
+</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2 col-md-offset-5">
+				<img class="image-profile" src="images/poto.jpeg" width="200px" height="200px"/><br>
+				<a href="https://github.com/Mafmudin">
+					<img src="images/github-logo.svg" width="16" height="16">
+				</a>
+
+				<a href="https://twitter.com/mafmudin">
+					<img src="images/twitter-logo-button.svg" width="16" height="16">
+				</a>
+
+				<a href="https://www.instagram.com/muchama_m/">
+					<img src="images/instagram-logo.svg" width="16" height="16">
+				</a>
+
+				<a href="https://plus.google.com/u/0/+MuchamadMafmudin">
+					<img src="images/google-plus-logo-button.svg" width="16" height="16">
+				</a>
+				
+				<br>
+				<a href="myrepo.html">Daftar Repositori</a><br>
+			</div>
+		</div>
+	</div>
+</div>
+<footer id="">
+	<div id="footer-content">&copy; 2018 Muchamad Mafmudin </br>
+		<a aria-label="Homepage" title="GitHub" class="footer-octicon" href="https://github.com">
+			<svg height="24" class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="24" aria-hidden="true">
+				<path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z">
+				</path>
+			</svg>
+		</a>
+	</div>
+</footer>
+
+</body>
+</html>
